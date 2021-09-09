8b64d786e312ddd764058c277eb4f57c07216bda
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-09-09 16:53:47 +0530

    update code

--- src/Webseries/GOT.jsx
+++ src/Webseries/GOT.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import { Navbar } from 'react-bootstrap';
-import Manu from '../MoviePages/Manu';
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./Webseries.css";
 
-const GOT = () =>{
-    return(
+const GOT = () => {
+  return (
     <>
-    <h1>Game of throns</h1>
+      <div className="series_body">
+        <h1>Game of Thrones</h1>
+        <p>2011 ‧ Drama ‧ 8 seasons</p>
+        <p>9.2/10 IMDb</p>
+        <p>89% Rotten Tomatoes</p>
+        <h6>
+          Nine noble families wage war against each other in order to gain
+          control over the mythical land of Westeros. Meanwhile, a force is
+          rising after millenniums and threatens the existence of living men.
+        </h6>
+        <img className="mt-5"
+          src="https://img1.hotstarext.com/image/upload/f_auto,t_hcdl/sources/r1/cms/prod/4904/474904-h"
+          alt="Logo"
+        />
+      </div>
     </>
-    )
-}
+  );
+};
 
 export default GOT;
